f8a8e769f7d9a6234e21bcbf6cf91d8876f87b6b
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-03-15 20:25:03 +0900

    Code 4.3 이동 평균 구하기_1

---
+++ 알고리즘_교재/코드4.3 이동 평균 구하기.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 이동평균 : N개의 측정치가 주어질 때 
+// 매달 M달 간의 이동 평균을 계산하는 프로그램
+
+vector<double> movingAverage1(const vector<double>& A, int M)
+{
+	vector<double> ret;
+	int N = A.size();
+	for (int i = M - 1; i < N; ++i) {
+		// A[i]까지의 이동 평균 구하기
+		double partialSum = 0;
+		for (int j = 0; j < M; ++j) {
+			partialSum += A[i - j];
+			ret.push_back(partialSum / M);
+		}
+	}
+	return ret;
+}
+
+int main(void)
+{
+	vector<double> A = {1,2,3,4,5,6,7,8,9,10,11,12};
+	vector<double> Result;
+	int M = 3;
+	Result = movingAverage1(A,3);
+	
+	for (int i = 0; i < Result.size(); i++) {
+		cout << Result[i] << endl;
+	}
+
+
+	return 0;
+}
